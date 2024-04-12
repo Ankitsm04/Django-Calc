35e519bd8e1314b482f0f095dfde289bdbcb86d0
Ankitsm38 <ankitsm38@gmail.com>
2024-04-12 20:56:54 +0530

    update

--- Calc/wsgi.py
+++ Calc/wsgi.py
@@ -1,7 +1,7 @@
 
 import os
 from django.core.wsgi import get_wsgi_application
-settings_module = 'Calc.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'Calc.settings'
+settings_module = 'Calc.deployement' if 'WEBSITE_HOSTNAME' in os.environ else 'Calc.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
